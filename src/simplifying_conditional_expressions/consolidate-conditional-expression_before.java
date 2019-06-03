5c05071e9f0aae9993fc7c819143e1e8569dd751
Jeanlee23 <jeanlee-roy@hotmail.com>
2019-06-03 10:11:22 -0500

    Decompose

--- src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
+++ src/simplifying_conditional_expressions/consolidate-conditional-expression_before.java
@@ -2,5 +2,5 @@ double disabilityAmount() {
   if (seniority < 2 ||monthsDisabled > 12 || isPartTime) {
     return 0;
   }
- 
+
 }
--- src/simplifying_conditional_expressions/decompose-conditional_before.java
+++ src/simplifying_conditional_expressions/decompose-conditional_before.java
@@ -1,6 +1,12 @@
-if (date.before(SUMMER_START) || date.after(SUMMER_END)) {
+if (!isSummer) {
   charge = quantity * winterRate + winterServiceCharge;
 }
 else {
   charge = quantity * summerRate;
 }
+
+bool isSummer()
+        {
+            if(date.before(SUMMER_START) || date.after(SUMMER_END){return 0;}
+            else return 1;
+        }
