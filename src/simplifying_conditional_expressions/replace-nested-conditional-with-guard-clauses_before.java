fa68cd7ec8594bd5d74f80e8c45e15fdc43544ed
Jeanlee23 <jeanlee-roy@hotmail.com>
2019-06-03 10:18:33 -0500

    Nested Conditional

--- src/simplifying_conditional_expressions/replace-nested-conditional-with-guard-clauses_before.java
+++ src/simplifying_conditional_expressions/replace-nested-conditional-with-guard-clauses_before.java
@@ -3,18 +3,14 @@ public double getPayAmount() {
   if (isDead){
     result = deadAmount();
   }
-  else {
-    if (isSeparated){
+  else if(isSeparated){
       result = separatedAmount();
     }
-    else {
-      if (isRetired){
+    else if(isRetired){
         result = retiredAmount();
-      }
-      else{
+    }
+        else{
         result = normalPayAmount();
       }
-    }
-  }
   return result;
 }
